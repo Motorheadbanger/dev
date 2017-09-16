c426a490cee1a2e5f97eb49f2272b245bea2b006
Motorheadbanger <kneecrush@gmail.com>
2017-09-16 20:17:50 +0300

    Moving sources

--- thinking/src/Sixty_1.java
+++
@@ -1,8 +0,0 @@
-public class Sixty_1 {
-    static int i;
-    static char c;
-    public static void main(String[] args) {
-        System.out.println("int = " + i);
-        System.out.println("char = " + c);
-    }
-}
--- thinking/src/Sixty_3.java
+++
@@ -1,6 +0,0 @@
-public class Sixty_3 {
-    class ATypeName { ATypeName a = new ATypeName(); }
-    public static void main(String[] args) {
-
-    }
-}
