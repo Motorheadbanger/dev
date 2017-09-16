4d4d183ceba49de5c4924811a62d784e958e91be
Motorheadbanger <kneecrush@gmail.com>
2017-09-16 21:12:42 +0300

    Exercise 4 on Objects

---
+++ thinking/Objects/Velocity.java
@@ -0,0 +1,9 @@
+public class Velocity {
+    final static int distance = 100;
+    final static int time = 10;
+
+    public static void main(String[] args) {
+        int velocity = distance / time;
+        System.out.println("If the distance travelled is " + distance + " meters, covered in " + time + " seconds, then the velocity is " + velocity + " m/s");
+    }
+}
