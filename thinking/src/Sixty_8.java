ec0cf864468d38d5e636f7a66360488108c571a4
Motorheadbanger <kneecrush@gmail.com>
2017-09-16 19:33:15 +0300

    Exercise 8 on page 60

---
+++ thinking/src/Sixty_8.java
@@ -0,0 +1,10 @@
+public class Sixty_8 {
+    static int i = 0;
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Sixty_8 s = new Sixty_8();
+            s.i++;
+        }
+        System.out.println(i);
+    }
+}
